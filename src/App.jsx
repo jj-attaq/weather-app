cf14c8e39109ea1caa6fce84fd69f0f2cd8e1dd2
jj.attaq <jj.attaq@tutanota.com>
2023-07-15 16:44:54 +0200

    fetching and displaying work properly now

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Button from './components/button.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,31 +8,32 @@ const log = console.log;
 
 function App() {
     const [loc, setLoc] = useState('');
-    const [fetchedData, setFetchedData] = useState('{"result":true, "count":42}')
-    async function getData(location) {
-        location = loc;
-        const url = await fetch(`http://api.weatherapi.com/v1/current.json?key=b86be9f2d5d24cd3a8081721231507&q=${location}`, {mode: 'cors'});
-        const data = await url.json();
-        await log(data)
-        await setFetchedData(JSON.stringify(data));
-    }
-    const data = JSON.parse(fetchedData);
+    const [fetchedData, setFetchedData] = useState([])
+    useEffect(() => {
+        if (loc === '') {
+            return
+        } else if (loc !== '') {
+            fetch(`http://api.weatherapi.com/v1/current.json?key=b86be9f2d5d24cd3a8081721231507&q=${loc}`, {mode: 'cors'})
+                .then(response => response.json())
+                .then(json => {
+                    setFetchedData([json]);
+                    log(json);
+                })
+        }
+    }, [loc]);
 
     const displayTemp = () => { 
-        if (data.current == undefined) {
+        if (fetchedData[0] === undefined) {
+            log('oops')
             return ''
         } else {
-            return `The temperature is ${data.current.temp_c} degrees.`
+            return `The temperature is ${fetchedData[0].current.temp_c} degrees.`
         }
-    }
-    async function buttonGoVroom() {
-        await setLoc('Belgrade')
-        await getData()
     }
 
     return (
         <>
-        <Button style="btn" className="test" value="Change Location" onBtnClick={() => buttonGoVroom()}/>
+        <Button style="btn" className="test" value="Change Location" onBtnClick={() => setLoc(prompt())}/>
         <div>{loc}</div>
         <div>{displayTemp()}</div>
         </>
